6a668e09138ce02e7a1d341bd28efa26f234e205
Parker Mathis <parkerjmathis@gmail.com>
2023-02-25 16:50:55 -0800

    Added basic contact form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,5 +1,29 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import "../../styles/content.css";
 
 export default function Contact() {
-  return <div>Contact</div>;
+  return (
+    <Form className="content">
+      <Form.Group className="mb-3">
+        <Form.Label>Name</Form.Label>
+        <Form.Control type="text" placeholder="Enter name" />
+      </Form.Group>
+
+      <Form.Group className="mb-3">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+      </Form.Group>
+
+      <Form.Group className="mb-3">
+        <Form.Label>Message</Form.Label>
+        <Form.Control as="textarea" placeholder="Leave a message here" style={{ height: "100px" }} />
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
 }
