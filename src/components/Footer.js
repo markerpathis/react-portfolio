cc9dfd72c9df4c5fe188987bccc544a0bcc4d6cb
Parker Mathis <parkerjmathis@gmail.com>
2023-02-28 17:40:51 -0800

    Added Google icon to footer for email requests

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,17 @@
 import React from "react";
 import logoGitHub from "../assets/logoGitHub.png";
 import logoLinkedIn from "../assets/logoLinkedIn.png";
+import logoGoogle from "../assets/logoGoogle.png";
 
 const styles = {
   footer: {
     display: "flex",
     justifyContent: "center",
+    alignItems: "center",
     paddingTop: "50px",
+  },
+  icons: {
+    paddingLeft: "15px",
   },
 };
 
@@ -14,13 +19,18 @@ export default function Footer() {
   return (
     <div style={styles.footer}>
       <div>
-        <a href="https://github.com/markerpathis" target="_blank" rel="noreferrer">
+        <a href="https://github.com/markerpathis" target="_blank" rel="noreferrer" style={styles.icons}>
           <img src={logoGitHub} alt="GitHub Logo" height="50px" />
         </a>
       </div>
       <div>
-        <a href="https://www.linkedin.com/in/parker-mathis/" target="_blank" rel="noreferrer">
+        <a href="https://www.linkedin.com/in/parker-mathis/" target="_blank" rel="noreferrer" style={styles.icons}>
           <img src={logoLinkedIn} alt="LinkedIn Logo" height="50px" />
+        </a>
+      </div>
+      <div>
+        <a href="mailto:parkerjmathis@gmail.com?subject=Developer Portfolio Contact Request" target="_blank" rel="noreferrer" style={styles.icons}>
+          <img src={logoGoogle} alt="Google Logo" height="65px" />
         </a>
       </div>
     </div>
